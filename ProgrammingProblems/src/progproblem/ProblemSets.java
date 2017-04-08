fbd7f057a6049149b5829199f9fab6137d5a4bf6
ShlokD <shlokpdesai@gmail.com>
2017-04-08 16:51:42 -0700

    Added three problems

--- ProgrammingProblems/src/progproblem/ProblemSets.java
+++ ProgrammingProblems/src/progproblem/ProblemSets.java
@@ -1,7 +1,68 @@
 package progproblem;
 
+import java.util.HashMap;
+
+import org.junit.experimental.max.MaxCore;
+
 public class ProblemSets {
-	public int echo(int x) {
-		return x;
+	public int[] pairWithGivenSum(int[] numbers, int sum) {
+		int[] result = { -1, -1 };
+		HashMap<Integer, Integer> sumMap = new HashMap<>();
+		for(int i = 0; i<numbers.length; ++i) {
+			if(sumMap.containsKey(sum - numbers[i])) {
+				result[0] =  sumMap.get(sum -  numbers[i]).intValue();
+				result[1] = i;
+			} else {
+				sumMap.put(numbers[i], i);
+			}
+		}
+		
+		return result;
+	}
+	
+	public int[] binaryArraySort(int[] binaryArray) {
+		int[] sortedArray = new int[binaryArray.length];
+		int zeroCount = 0;
+		
+		for(int i=0; i<binaryArray.length; ++i) {
+			if(binaryArray[i] == 0) {
+				zeroCount++;
+			}
+		}
+		
+		for(int i=0; i<sortedArray.length; ++i) {
+			if(zeroCount-- > 0) {
+				sortedArray[i] = 0;
+			} else {
+				sortedArray[i] = 1;
+			}
+		}
+		return sortedArray;
+	}
+	
+	public int longestCommonSubsequence(String first, String second) {
+		int firstLength = first.length();
+		int secondLength = second.length();
+		
+		int[][] matchTable = new int[firstLength + 1][secondLength + 1];
+		
+		for(int i = 0; i<=firstLength; ++i) {
+			matchTable[i][0] = 0;
+		}
+		
+		for(int i=0; i<=secondLength; ++i) {
+			matchTable[0][i] = 0;
+		}
+		
+		for(int i = 1; i<=firstLength; ++i) {
+			for(int j = 1; j<=secondLength; ++j) {
+				if(first.charAt(i-1) == second.charAt(j - 1)) {
+					matchTable[i][j] = matchTable[i-1][j-1] + 1;
+				} else {
+					matchTable[i][j] = Math.max(matchTable[i-1][j], matchTable[i][j-1]);
+				}
+			}
+		}
+		return matchTable[firstLength][secondLength];
 	}
 }
--- ProgrammingProblems/src/progproblem/ProblemSetsTest.java
+++ ProgrammingProblems/src/progproblem/ProblemSetsTest.java
@@ -2,14 +2,36 @@ package progproblem;
 
 import static org.junit.Assert.*;
 
+import java.nio.channels.Pipe;
+
 import org.junit.Test;
 
 public class ProblemSetsTest {
 
 	@Test
-	public void echoTest() {
+	public void pairWithGivenSumTest() {
 		ProblemSets pSets = new ProblemSets();
-		assertEquals(10, pSets.echo(10));
+		int[] numbers = {-4, 6, 8, 1, 3, 9, 3};
+		int sum = 10;
+		int[] expected = {3, 5};
+
+		assertArrayEquals(expected, pSets.pairWithGivenSum(numbers, sum));
+	}
+	
+	@Test
+	public void sortBinaryArrayTest() {
+		ProblemSets pSets = new ProblemSets();
+		int[] unsortedBinaryArray = {1, 0, 1, 0, 0, 1, 0};
+		int[] sortedBinaryArray = { 0, 0, 0, 0, 1, 1, 1};
+		assertArrayEquals(sortedBinaryArray, pSets.binaryArraySort(unsortedBinaryArray));
+	}
+	
+	@Test
+	public void longestCommonSubsequenceTest() {
+		ProblemSets pSets = new ProblemSets();
+		String first = "BCADCBADC";
+		String second = "BAACDADCB";
+		assertEquals(6, pSets.longestCommonSubsequence(first, second));
 	}
 
 }
